579bac84fa4cc3b559ff48afc5346a81a245284f
Aakash Pereira <25438913+TheDarthHazard@users.noreply.github.com>
2024-06-18 19:05:02 +0100

    Buyer feature implemented
    - CSS updated to style buyers name to individual items. Select element styling defined.
    - HTML updated to add select listbox and form element.
    - JS updated to implement buyer feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,17 +10,26 @@ const database = getDatabase(app)
 const itemsInDB = ref(database, "shoppingList")
 
 const addItemBtn = document.getElementById("add-item-btn")
+
+const shoppingForm = document.getElementById("shopping-form")
+
 const itemInput = document.getElementById("item-input")
+const buyerInput = document.getElementById("buyer-input")
 
 const itemsContainer = document.getElementById("items-container")
 
-addItemBtn.addEventListener("click", function() {
-    addItemToList(itemInput.value)
+shoppingForm.addEventListener("submit", function(e) {
+    e.preventDefault()
+    addItemToList(itemInput.value, buyerInput.value)
 })
 
-function addItemToList(value) {
+function addItemToList(value, recipient) {
     if(value.length > 0) {
-        push(itemsInDB, value)
+        let entry = {
+            item: value,
+            buyer: recipient
+        }
+        push(itemsInDB, entry)
         handleAfterEntry()
     } else {
         alert("Enter a shopping item to add to the list")
@@ -35,29 +44,54 @@ function handleAfterEntry() {
 onValue(itemsInDB, function(snapshot) {
     if (snapshot.exists()) {
         let itemsArray = Object.entries(snapshot.val())
-    clearContainer()
 
-    for (let i = 0; i < itemsArray.length; i++) {
-        let itemID = itemsArray[i][0]
-        let itemValue = itemsArray[i][1]
-        
-        let liEl = document.createElement("li")
-        liEl.classList.add("item")
+        console.log(itemsArray)
 
-        let buttonEl = document.createElement("button")
-        buttonEl.textContent = itemValue
-        buttonEl.setAttribute("aria-label", `Remove ${itemValue}`)
+        clearContainer()
 
-        liEl.insertAdjacentElement("beforeend", buttonEl)
+        for (let i = 0; i < itemsArray.length; i++) {
+            let itemID = itemsArray[i][0]
+            let itemValue = itemsArray[i][1].item
+            let itemBuyer = itemsArray[i][1].buyer
+            
+            let liEl = document.createElement("li")
+            liEl.classList.add("item")
 
-        liEl.addEventListener("click", function() {
-            if (confirm("Are you sure you've bought this item?")) {
-                let exactItemLocation = ref(database, `shoppingList/${itemID}`)
-                remove(exactItemLocation)
+            let nameEl = document.createElement("span")
+            nameEl.textContent = itemBuyer
+            nameEl.classList.add("buyer")
+            
+            switch(itemBuyer) {
+                case "Adrian":
+                    nameEl.classList.add("buyer-adrian")
+                    break
+                case "Aakash":
+                    nameEl.classList.add("buyer-aakash")
+                    break
+                case "Eraisha":
+                    nameEl.classList.add("buyer-eraisha")
+                    break
+                case "Pruthuvi":
+                    nameEl.classList.add("buyer-pruthuvi")
+                    break
             }
-        })
-        itemsContainer.insertAdjacentElement("beforeend", liEl)
-    }
+
+            liEl.append(nameEl)
+
+            let buttonEl = document.createElement("button")
+            buttonEl.textContent = itemValue
+            buttonEl.setAttribute("aria-label", `Remove ${itemValue}`)
+
+            liEl.insertAdjacentElement("beforeend", buttonEl)
+
+            liEl.addEventListener("click", function() {
+                if (confirm("Are you sure you've bought this item?")) {
+                    let exactItemLocation = ref(database, `shoppingList/${itemID}`)
+                    remove(exactItemLocation)
+                }
+            })
+            itemsContainer.insertAdjacentElement("beforeend", liEl)
+        }
     } else {
         itemsContainer.innerHTML = "<li>There are no items to buy right now...</li>"
     }
